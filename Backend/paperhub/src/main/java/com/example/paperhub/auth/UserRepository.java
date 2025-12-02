cc870e0472418ade0f79b118f287f5bbf8b39388
unnamedhoead <1368529505@qq.com>
2025-12-02 19:02:53 +0800

    [登录注册]解决坏账号和密码小眼睛问题

--- Backend/paperhub/src/main/java/com/example/paperhub/auth/UserRepository.java
+++ Backend/paperhub/src/main/java/com/example/paperhub/auth/UserRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {//定义用户仓库
     Optional<User> findByEmail(String email);//根据邮箱查找用户
     boolean existsByEmail(String email);//判断用户是否存在
+    boolean existsByEmailAndVerified(String email, boolean verified);//判断已验证用户是否存在
     // 根据名字模糊搜索用户
     java.util.List<User> findByNameContainingIgnoreCase(String name);
 }
